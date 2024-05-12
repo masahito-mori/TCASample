e479c6ba48b0900fec4408ed7e5782f5ebd8c4f3
masahito-mori <mmori.0426@gmail.com>
2024-05-12 19:24:53 +0900

    👍[TCAチュートリアル]Chapter1-3:Testing your feature
    【対応内容】
    ・Chapter1-3:Testing your feature

---
+++ TCASampleTests/CounterFeatureTests.swift
@@ -0,0 +1,69 @@
+//
+//  CounterFeatureTests.swift
+//  TCASampleTests
+//
+//  Created by Masahito Mori on 2024/05/12.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import TCASample
+
+@MainActor
+final class CounterFeatureTests: XCTestCase {
+    func testCounter() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+        
+        await store.send(.incrementButtonTapped) {
+            $0.count = 1
+        }
+        await store.send(.incrementButtonTapped) {
+            $0.count = 2
+        }
+        await store.send(.decrementButtonTapped) {
+            $0.count = 1
+        }
+    }
+    
+    func testTimer() async {
+        let clock = TestClock()
+        
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+        
+        await clock.advance(by: .seconds(5))
+        await store.receive(\.timerTick) {
+            $0.count = 1
+        }
+        
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = false
+        }
+    }
+    
+    func testNumberFact() async {
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: {
+            $0.numberFact.fetch = { "\($0) is a good number." }
+        }
+        await store.send(.factButtonTapped) {
+            $0.isLoading = true
+        }
+        
+        await store.receive(\.factResponse) {
+            $0.isLoading = false
+            $0.fact = "0 is a good number."
+        }
+    }
+}
