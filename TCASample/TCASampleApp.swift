00047fe9340f324ba882730f813200805fb20053
masahito-mori <mmori.0426@gmail.com>
2024-05-12 20:28:59 +0900

    👍[TCAチュートリアル]Chapter1-4:Composing features
    【対応内容】
    ・Chapter1-4:Composing features

--- TCASample/TCASampleApp.swift
+++ TCASample/TCASampleApp.swift
@@ -10,14 +10,14 @@ import SwiftUI
 
 @main
 struct TCASampleApp: App {
-    static let store = Store(initialState: CounterFeature.State()) {
-        CounterFeature()
+    static let store = Store(initialState: AppFeature.State()) {
+        AppFeature()
             ._printChanges()
     }
     
     var body: some Scene {
         WindowGroup {
-            CounterView(store: TCASampleApp.store)
+            AppView(store: TCASampleApp.store)
         }
     }
 }
